fec8721bafdc81c4a90b040d965b452b44405347
Mateo-M <me.mateomeana@gmail.com>
2019-02-03 09:49:29 +0100

    import custom-electron-titlebar & init custom titlebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ElectronService } from './providers/electron.service';
 import { TranslateService } from '@ngx-translate/core';
 import { AppConfig } from '../environments/environment';
+import { Titlebar, Themebar, Color } from 'custom-electron-titlebar';
+import { app, remote } from 'electron';
+
+const { Menu, MenuItem } = remote;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(public electronService: ElectronService,
     private translate: TranslateService) {
 
@@ -22,5 +26,13 @@ export class AppComponent {
     } else {
       console.log('Mode web');
     }
+  }
+
+  ngOnInit() {
+    const titlebar = new Titlebar({
+      backgroundColor: Color.TRANSPARENT,
+      maximizable: false,
+      menu: new Menu()
+    });
   }
 }
