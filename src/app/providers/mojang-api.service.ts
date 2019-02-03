d6fc64334ba53e3e996ce86870a0fea4fd607807
Mateo-M <me.mateomeana@gmail.com>
2019-02-03 09:48:54 +0100

    remove import, comment getIp method & add validateAccessToken function

--- src/app/providers/mojang-api.service.ts
+++ src/app/providers/mojang-api.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { HttpHeaders } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
 import { Router } from '@angular/router';
 
@@ -12,24 +11,41 @@ export class MojangApiService {
 
   constructor(private http: HttpClient, private router: Router) { }
 
-  getIp() {
+  /*getIp() {
     return this.http.get('https://api.ipify.org?format=json');
-  }
+  }*/
 
   signin(signinObj) {
     return this.http.post('https://authserver.mojang.com/authenticate', signinObj);
   }
+
   signout() {
     const signoutObj = {
       'accessToken': localStorage.getItem('accessToken'),
       'clientToken': localStorage.getItem('clientToken')
     };
     return this.http.post('https://authserver.mojang.com/invalidate', signoutObj)
-    .pipe(
-      tap(() => {
-      localStorage.removeItem('accessToken'),
-      localStorage.removeItem('clientToken');
-      this.router.navigate(['/']);
-    }));
+      .pipe(
+        tap(() => {
+          localStorage.removeItem('accessToken'),
+            localStorage.removeItem('clientToken');
+          this.router.navigate(['/']);
+        }));
+  }
+
+  valideAccessToken() {
+    const validateAccessTokenObj = {
+      'accessToken': localStorage.getItem('accessToken'),
+      'clientToken': localStorage.getItem('clientToken')
+    };
+    return this.http.post('https://authserver.mojang.com/validate', validateAccessTokenObj);
+  }
+
+  refreshAccessToken() {
+    const refreshAccessTokenObj = {
+      'accessToken': localStorage.getItem('accessToken'),
+      'clientToken': localStorage.getItem('clientToken')
+    };
+    return this.http.post('https://authserver.mojang.com/refresh', refreshAccessTokenObj);
   }
 }
